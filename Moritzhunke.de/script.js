db4b278dddecc978378ba8d4882c1ab5dd5e3753
Sebastian Haiduk <haidseba@gmail.com>
2018-08-28 17:59:00 +0200

    beautifyed code

--- Moritzhunke.de/script.js
+++ Moritzhunke.de/script.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-    $('.fadeIn').delay(1000).fadeIn(1000);
+  $('.fadeIn').delay(1000).fadeIn(1000);
 
-    $(window).scroll( function(){
+  $(window).scroll(function() {
 
-        $('.portfolio-content').each( function(i){
+    $('.portfolio-content').each(function(i) {
 
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if( bottom_of_window > bottom_of_object ){
+      if (bottom_of_window > bottom_of_object) {
 
-                $(this).animate({'opacity':'1'},500);
+        $(this).animate({
+          'opacity': '1'
+        }, 500);
 
-            }
-
-        });
+      }
 
     });
+
+  });
 
 });
