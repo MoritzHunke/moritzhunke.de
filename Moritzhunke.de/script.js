434c66e5d63699852c704e48914b405eae661e90
Moritz Hunke <moritz.hunke@stud.hshl.de>
2018-09-03 17:27:08 +0200

    fixes Animation

--- Moritzhunke.de/script.js
+++ Moritzhunke.de/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('.fadeIn').delay(1000).fadeIn(1000);
+  $('.fadeIn').delay(500).fadeIn(500);
 
   $(window).scroll(function() {
 
     $('.portfolio-content').each(function(i) {
 
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top /*+ $(this).outerHeight()*/;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       if (bottom_of_window > bottom_of_object) {
 
-        $(this).animate({
+        $(this).delay(250).animate({
           'opacity': '1'
         }, 500);
 
